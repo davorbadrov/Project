af33789a460b9bef4b5160fa3f713cc42a1ace7c
Davor Badrov <davor.badrov@hotmail.com>
2012-02-13 22:18:22 +0100

    minor changes, refactoring etc.

--- codeHighlight/chProject/WebContent/scripts/highlight.js
+++ codeHighlight/chProject/WebContent/scripts/highlight.js
@@ -5,8 +5,7 @@ function includeScripts()
 		'<script type="text/javascript" src="scripts/shCore.js"></script>' +
 		'<script type="text/javascript" src="scripts/shBrushJScript.js"></script>' +
 		'<link href="styles/shCore.css" rel="stylesheet" type="text/css" />' +
-		'<link href="styles/shThemeDefault.css" rel="stylesheet" type="text/css" />' +
-		'<script type="text/javascript">SyntaxHighlighter.highlight();</script>';
+		'<link href="styles/shThemeDefault.css" rel="stylesheet" type="text/css" />';
 	 
 	document.write(shScripts);
 }
@@ -18,27 +17,21 @@ function highlightCode()
 	var inputArea = document.getElementById("codeInput");
 	var outputArea = document.getElementById("codeOutput");
 
+	if(inputArea.value == "")
+	{
+		alert("The code area is empty!");
+		return;
+	}
+	
 	var codeDiv = document.createElement("div");
 	codeDiv.setAttribute("id", "codeOutput");
 	
 	var codePre = document.createElement("pre");
 	codePre.setAttribute("class", "brush: js");
 	
-	var code;
-	
-	console.log("value: " + inputArea.value);
-	console.log("default valeu: " + inputArea.defaultValue);
-	
-	if(inputArea.value != "")
-		code = document.createTextNode(inputArea.value);
-	else
-		{
-		code = document.createTextNode(inputArea.defaultValue);
-		alert(inputArea.defaultValue);	
-		}
+	var code = document.createTextNode(inputArea.value);
 	
 	codePre.appendChild(code);
-
 	codeDiv.appendChild(codePre);
 	
 	var codeArea = document.getElementById("codeContainer");
